f3e804f3dcc6e98090c781baa34a77b0ffb4975b
bendunhampdx <bendunhampdx@gmail.com>
2021-09-21 11:31:34 -0700

    pass test determining if word is an array
    Co-authored-by: Melissa Schatz-Miller <melissa.scmi@gmail.com>

--- Palindromes.Tests/ModelTests/WordTests.cs
+++ Palindromes.Tests/ModelTests/WordTests.cs
@@ -6,6 +6,14 @@ namespace Palindromes.Tests
   [TestClass]
   public class WordTests
   {
+  
+  [TestMethod]
+  public void IsPalindrome_IsArray_True()
+  {
+    Word testWord = new Word();
+    Assert.AreEqual(true, testWord.IsPalindrome("butter"));
+  }
+
   [TestMethod]
   // public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
     public void IsPalindrome_IsSameReversed_True()
